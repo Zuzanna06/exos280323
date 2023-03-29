e13601e24ec0a76289ada84dec0c2926419cccd8
Zuzanna06 <103110190+Zuzanna06@users.noreply.github.com>
2023-03-29 18:45:01 +0200

    firstn commit

---
+++ main.js
@@ -0,0 +1,63 @@
+// const myArray = ["A", "B", "C", "D"];
+// console.log("longeur du tableau: ", myArray.length);
+// console.log("le premier element: ", myArray[0]);
+// console.log("le second element: ", myArray[1]);
+// console.log("le troiseme: ", myArray[2]);
+// console.log(myArray[myArray.length - 1]);
+
+// for(il y a ça; tant que; fais)
+// for(il y a i=0; tant que i<tableau.length; alors fais i++)
+// for(i=0; i<tableau.length; i++)
+
+// for(i = 0; i < myArray.length; index++) {
+//     let para = document.createElement('p')
+//     para.append(myArray[i])
+//     document.body.append(para)
+
+//     //console.log(myArray[i] + "A");
+//     //console.log(i);
+// }
+
+//for-in for-of  // valeur la clef
+
+// for (const element in myArray) {
+//   console.log(element);
+// }
+// // ici le clef de objet
+
+// for (const element of myArray) {
+//   console.log(element); // A B C D
+// }
+// ici la valeur de objet
+
+// inner remplace
+//const para = document.querySelector('#teste')
+//rpara append ajuter
+//  para.append('test')
+
+// para.innerText = '<div>test<div>'
+// para.innerHTML = '<div>test<div>'
+
+// let chiffre = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let index = 0; index < chiffre.length; index++) {
+//     let div = document.createElement('div');
+//     let h3 = document.createElement('h3')
+//     h3.append("Table de " + (index + 1));
+//     div.append(h3);
+//     document.body.append(div)
+//   //console.loge(index)
+//   //console.log(index + 1); //index + 1
+//   console.log("table de " + (index + 1)); //concatenation
+
+//   for (let i = 0; i < chiffre.length; i++) {
+//     let para = document.createElement('p')
+//     para.append(chiffre[i] + "x" + (index + 1) + "=" + (index + 1 ) * chiffre[i]);
+//     div.append(para)
+//     console.log(chiffre[i] + "x" + (index + 1) + "=" + (index + 1 ) * chiffre[i]);
+//   }
+// }
+
+//dans cet element(div) met(append) celui là(p)
+
+// div.append(p)
